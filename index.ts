ed8c972e344b768cadb297cf84879916a067c727
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-01-30 17:34:13 -0700

    Create object interface

--- index.ts
+++ index.ts
@@ -2,9 +2,19 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+// interface defines structure of an object
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios
   .get(url)
   .then((response) => {
-    console.log(response.data);
+    // tell typescript response.data is a Todo
+    const todo = response.data as Todo;
+    const { id, title, completed } = todo;
+    console.log(`ID ${id}, title: ${title}, completed: ${completed}`);
   })
   .catch((error) => console.log(error));
